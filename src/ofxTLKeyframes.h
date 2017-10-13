903b21db024b1f79c84322df44bae53c5f67fb56
mark <mark@fusefactory.it>
2017-10-13 23:36:24 +0200

    get/setGridSections methods for ofxTLKeyframes (and more consistent indents)

--- src/ofxTLKeyframes.h
+++ src/ofxTLKeyframes.h
@@ -39,60 +39,60 @@
 
 class ofxTLKeyframe {
   public:
-	ofVec2f screenPosition; // cached screen position
-	unsigned long long previousTime; //for preventing overlap conflicts
-    unsigned long long time; //in millis
-    float value; //normalized
-    long grabTimeOffset;
-    float grabValueOffset;
+    ofVec2f screenPosition; // cached screen position
+    unsigned long long previousTime; //for preventing overlap conflicts
+      unsigned long long time; //in millis
+      float value; //normalized
+      long grabTimeOffset;
+      float grabValueOffset;
 };
 
 class ofxTLKeyframes : public ofxTLTrack
 {
   public:
-	ofxTLKeyframes();
-	virtual ~ofxTLKeyframes();
+    ofxTLKeyframes();
+    virtual ~ofxTLKeyframes();
 
-	virtual void draw();
+    virtual void draw();
 
-	virtual bool mousePressed(ofMouseEventArgs& args, long millis);
-	virtual void mouseMoved(ofMouseEventArgs& args, long millis);
-	virtual void mouseDragged(ofMouseEventArgs& args, long millis);
-	virtual void mouseReleased(ofMouseEventArgs& args, long millis);
+    virtual bool mousePressed(ofMouseEventArgs& args, long millis);
+    virtual void mouseMoved(ofMouseEventArgs& args, long millis);
+    virtual void mouseDragged(ofMouseEventArgs& args, long millis);
+    virtual void mouseReleased(ofMouseEventArgs& args, long millis);
 
-	virtual void nudgeBy(ofVec2f nudgePercent);
+    virtual void nudgeBy(ofVec2f nudgePercent);
 
-	virtual void keyPressed(ofKeyEventArgs& args);
+    virtual void keyPressed(ofKeyEventArgs& args);
 
-	virtual void getSnappingPoints(set<unsigned long long>& points);
+    virtual void getSnappingPoints(set<unsigned long long>& points);
 
-	virtual void save();
-	virtual void load();
+    virtual void save();
+    virtual void load();
 
-	virtual void clear();
+    virtual void clear();
 
-	virtual void addKeyframe();
-	virtual void addKeyframe(float value);
-	virtual void addKeyframeAtMillis(unsigned long long millis);
-	virtual void addKeyframeAtMillis(float value, unsigned long long millis);
-	virtual void simplifySelectedKeyframes( float tolerance = 0.01f);
+    virtual void addKeyframe();
+    virtual void addKeyframe(float value);
+    virtual void addKeyframeAtMillis(unsigned long long millis);
+    virtual void addKeyframeAtMillis(float value, unsigned long long millis);
+    virtual void simplifySelectedKeyframes( float tolerance = 0.01f);
     ofxTLKeyframe* getKeyframeAtMillis( unsigned long long millis);
 
     vector<ofxTLKeyframe*>& getKeyframes();
 
-	//copy paste
-	virtual string copyRequest();
-	virtual string cutRequest();
-	virtual void pasteSent(string pasteboard);
-	virtual void selectAll();
-	virtual void unselectAll();
+    //copy paste
+    virtual string copyRequest();
+    virtual string cutRequest();
+    virtual void pasteSent(string pasteboard);
+    virtual void selectAll();
+    virtual void unselectAll();
 
     virtual int getSelectedItemCount();
 
-	virtual unsigned long long getEarliestTime();
-	virtual unsigned long long getLatestTime();
-	virtual unsigned long long getEarliestSelectedTime();
-	virtual unsigned long long getLatestSelectedTime();
+    virtual unsigned long long getEarliestTime();
+    virtual unsigned long long getLatestTime();
+    virtual unsigned long long getEarliestSelectedTime();
+    virtual unsigned long long getLatestSelectedTime();
 
     //undo
     virtual string getXMLRepresentation();
@@ -102,93 +102,96 @@ class ofxTLKeyframes : public ofxTLTrack
 
     virtual string getTrackType();
 
-	//sampling
-	virtual float getValue();
-	virtual float getValueAtPercent(float percent);
-	virtual float getValueAtTimeInMillis(long sampleTime);
+    //sampling
+    virtual float getValue();
+    virtual float getValueAtPercent(float percent);
+    virtual float getValueAtTimeInMillis(long sampleTime);
 
-	virtual void setValueRange(ofRange range, float defaultValue = 0);
-	virtual void setValueRangeMin(float min);
-	virtual void setValueRangeMax(float max);
-	virtual void setDefaultValue(float defaultValue);
+    virtual void setValueRange(ofRange range, float defaultValue = 0);
+    virtual void setValueRangeMin(float min);
+    virtual void setValueRangeMax(float max);
+    virtual void setDefaultValue(float defaultValue);
 
-	virtual void quantizeKeys(int step);
+    virtual void quantizeKeys(int step);
 
     virtual ofRange getValueRange();
 
-	//experimental binary saving. does not work with subclasses yet
-	void saveToBinaryFile();
-	void loadFromBinaryFile();
-	bool useBinarySave;
+    //experimental binary saving. does not work with subclasses yet
+    void saveToBinaryFile();
+    void loadFromBinaryFile();
+    bool useBinarySave;
+
+    void setGridSections(unsigned int amount){ this->gridSections = amount; }
+    unsigned int getGridSections() const { return this->gridSections; }
 
   protected:
-	virtual ofxTLKeyframe* newKeyframe();
-	vector<ofxTLKeyframe*> keyframes;
+    virtual ofxTLKeyframe* newKeyframe();
+    vector<ofxTLKeyframe*> keyframes;
 
-	//cached previews for fast drawing of large timelines
-	ofPolyline preview;
-	vector<ofVec2f> keyPoints;
+    //cached previews for fast drawing of large timelines
+    ofPolyline preview;
+    vector<ofVec2f> keyPoints;
 
-	virtual void recomputePreviews();
-	bool shouldRecomputePreviews;
+    virtual void recomputePreviews();
+    bool shouldRecomputePreviews;
 
-	virtual float sampleAtPercent(float percent); //less accurate than millis
+    virtual float sampleAtPercent(float percent); //less accurate than millis
     virtual float sampleAtTime(long sampleTime);
-	virtual float interpolateValueForKeys(ofxTLKeyframe* start,ofxTLKeyframe* end, unsigned long long sampleTime);
-	virtual float evaluateKeyframeAtTime(ofxTLKeyframe* key, unsigned long long sampleTime, bool firstKey = false);
+    virtual float interpolateValueForKeys(ofxTLKeyframe* start,ofxTLKeyframe* end, unsigned long long sampleTime);
+    virtual float evaluateKeyframeAtTime(ofxTLKeyframe* key, unsigned long long sampleTime, bool firstKey = false);
 
     ofRange valueRange;
-	float defaultValue;
+    float defaultValue;
 
-	//keep these stored for efficient search through the keyframe array
-	int lastKeyframeIndex;
-	unsigned long long lastSampleTime;
+    //keep these stored for efficient search through the keyframe array
+    int lastKeyframeIndex;
+    unsigned long long lastSampleTime;
 
     virtual ofxTLKeyframe* keyframeAtScreenpoint(ofVec2f p);
-	bool isKeyframeIsInBounds(ofxTLKeyframe* key);
-	bool isKeyframeSelected(ofxTLKeyframe* k);
+    bool isKeyframeIsInBounds(ofxTLKeyframe* key);
+    bool isKeyframeSelected(ofxTLKeyframe* k);
     void selectKeyframe(ofxTLKeyframe* k);
     void deselectKeyframe(ofxTLKeyframe* k);
 
-	//don't override these in subclasses
-	void deleteSelectedKeyframes();
-	void deleteKeyframe(ofxTLKeyframe* keyframe);
-	//instead implement special behavior here:
-	//this is called before the keyframe is deleted and removed from the keyframes vector
-	virtual void willDeleteKeyframe(ofxTLKeyframe* keyframe){};
+    //don't override these in subclasses
+    void deleteSelectedKeyframes();
+    void deleteKeyframe(ofxTLKeyframe* keyframe);
+    //instead implement special behavior here:
+    //this is called before the keyframe is deleted and removed from the keyframes vector
+    virtual void willDeleteKeyframe(ofxTLKeyframe* keyframe){};
 
-	vector<ofxTLKeyframe*> selectedKeyframes;
+    vector<ofxTLKeyframe*> selectedKeyframes;
     ofxTLKeyframe* selectedKeyframe;
-	ofxTLKeyframe* hoverKeyframe;
+    ofxTLKeyframe* hoverKeyframe;
 
-	int selectedKeyframeIndex;
-	bool keysAreDraggable;
-	bool keysAreStretchable;
-	unsigned long long stretchAnchor;
-	unsigned long long stretchSelectPoint;
+    int selectedKeyframeIndex;
+    bool keysAreDraggable;
+    bool keysAreStretchable;
+    unsigned long long stretchAnchor;
+    unsigned long long stretchSelectPoint;
 
-	virtual void setKeyframeTime(ofxTLKeyframe* key, unsigned long long newTime);
-	virtual void updateKeyframeSort();
-	virtual void updateStretchOffsets(ofVec2f screenpoint, long grabMillis);
-	virtual void updateDragOffsets(ofVec2f screenpoint, long grabMillis);
+    virtual void setKeyframeTime(ofxTLKeyframe* key, unsigned long long newTime);
+    virtual void updateKeyframeSort();
+    virtual void updateStretchOffsets(ofVec2f screenpoint, long grabMillis);
+    virtual void updateDragOffsets(ofVec2f screenpoint, long grabMillis);
 
-	virtual string getXMLStringForKeyframes(vector<ofxTLKeyframe*>& keys);
-	virtual void createKeyframesFromXML(ofxXmlSettings xml, vector<ofxTLKeyframe*>& keyContainer);
-	virtual void restoreKeyframe(ofxTLKeyframe* key, ofxXmlSettings& xmlStore){};
+    virtual string getXMLStringForKeyframes(vector<ofxTLKeyframe*>& keys);
+    virtual void createKeyframesFromXML(ofxXmlSettings xml, vector<ofxTLKeyframe*>& keyContainer);
+    virtual void restoreKeyframe(ofxTLKeyframe* key, ofxXmlSettings& xmlStore){};
     virtual void storeKeyframe(ofxTLKeyframe* key, ofxXmlSettings& xmlStore){};
 
     virtual void selectedKeySecondaryClick(ofMouseEventArgs& args){};
 
 
-	bool screenpointIsInBounds(ofVec2f screenpoint);
-	ofVec2f screenPositionForKeyframe(ofxTLKeyframe* keyframe);
+    bool screenpointIsInBounds(ofVec2f screenpoint);
+    ofVec2f screenPositionForKeyframe(ofxTLKeyframe* keyframe);
 
     float screenYToValue(float screenY);
-	float valueToScreenY(float value);
+    float valueToScreenY(float value);
 
-	bool keysDidDrag;
-	bool keysDidNudge;
-	bool createNewOnMouseup;
+    bool keysDidDrag;
+    bool keysDidNudge;
+    bool createNewOnMouseup;
 
     float constrainVerticalDrag;
 
